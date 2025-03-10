ea71d42b7a4811145b5786f27d4a3e3d7ef8c2dc
farreltobias <garcia.farrel@gmail.com>
2025-03-10 10:17:18 -0300

    feat(cryptography): add abstractions and test fakes
    Introduce `Encrypter`, `HashGenerator`, and `HashComparer`
    abstract classes for cryptography. Add corresponding fake
    implementations for testing purposes.

---
+++ test/cryptography/fake-encrypter.ts
@@ -0,0 +1,7 @@
+import type { Encrypter } from '@/domain/system/application/cryptography/encrypter'
+
+export class FakeEncrypter implements Encrypter {
+  async encrypt(payload: Record<string, unknown>): Promise<string> {
+    return JSON.stringify(payload)
+  }
+}

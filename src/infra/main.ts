95058f2cb1c708f975b8d983ddf63be00e4f8799
farreltobias <garcia.farrel@gmail.com>
2025-03-21 14:44:30 -0300

    fix(config): update gitignore, env file, and server binding
    Updated .gitignore to include Terraform and GitHub secret files.
    Switched env_file in docker-compose.yml to use ".env".
    Set server to listen on all network interfaces.

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
 
-  await app.listen(port)
+  await app.listen(port, '0.0.0.0')
 }
 bootstrap()
